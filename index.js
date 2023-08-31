5ec0ff04ddeb10f5d3880171f5ef37e7e15915f5
Kevin <kevin_smith1292@yahoo.com>
2023-08-30 20:09:05 -0500

    strings

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'KEVIN'
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!'
+const shortGreeting = 'Welcome, ' + currentUser[0] + '!'
